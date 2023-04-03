fcf54f6af727f165e4b20739184822aa476458cc
Victor Hung <victorhung92@gmail.com>
2023-04-03 19:17:41 -0400

    Update 6_addressable_leds.ino

--- 6_addressable_leds/6_addressable_leds.ino
+++ 6_addressable_leds/6_addressable_leds.ino
@@ -19,7 +19,6 @@
 CRGB leds[NUM_LEDS];
 
 void setup() { 
-  Serial.begin(57600);
   Serial.println("resetting");
   FastLED.addLeds<WS2812,DATA_PIN,RGB>(leds,NUM_LEDS);
   FastLED.setBrightness(80);
@@ -29,7 +28,7 @@ void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } }
 
 void loop() { 
   static uint8_t hue = 0;
-  Serial.print("x");
+  
   // First slide the led in one direction
   for(int i = 0; i < NUM_LEDS; i++) {
     // Set the i'th led to red 
@@ -42,7 +41,6 @@ void loop() {
     // Wait a little bit before we loop around and do it again
     delay(10);
   }
-  Serial.print("x");
 
   // Now go in the other direction.  
   for(int i = (NUM_LEDS)-1; i >= 0; i--) {
